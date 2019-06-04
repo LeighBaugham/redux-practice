59f332c9e247e7227efac5a06fa523bf7bf0de39
Leigh Baugham <leigh.baugham@gmail.com>
2019-06-04 13:38:17 -0400

    first commit

---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,10 @@
+import { bindActionCreators } from "redux";
+
+// reducer takes in two things, action and copy of current state
+
+
+function posts(state = [], action) {
+    return state 
+}
+
+export default posts
